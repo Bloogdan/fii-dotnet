bc862a8f399f06f54c92490d65a475f8848e2623
Bloogdan <bogdanbadarau72@gmail.com>
2020-05-12 13:58:19 +0300

    Laborator 12/13 Mai

---
+++ Laborator WCF EF ASP.NET/ObjectWCF/InterfaceWCF.cs
@@ -0,0 +1,36 @@
+﻿using PostComment;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace ObjectWCF
+{
+    [ServiceContract]
+    interface InterfacePost
+    {
+        [OperationContract]
+        bool AddPost(Post post);
+        [OperationContract]
+        Post UpdatePost(Post post);
+        [OperationContract]
+        int DeletePost(int id);
+        [OperationContract]
+        Post GetPostById(int id);
+        [OperationContract]
+        List<Post> GetPosts();
+    }
+    [ServiceContract]
+    interface InterfaceComment
+    {
+        [OperationContract]
+        bool AddComment(Comment comment);
+        [OperationContract]
+        Comment UpdateComment(Comment newComment);
+        [OperationContract]
+        Comment GetCommentById(int id);
+    }
+    [ServiceContract]
+    interface IPostComment : InterfacePost, InterfaceComment
+    {
+    }
+
+}
